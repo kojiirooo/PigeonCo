0d407408c1f8b4aca7dad81cc65fcf2b808db439
johnrich9930 <johnrichadante45@gmail.com>
2026-09-24 18:39:51 +0800

    pigeon class update

---
+++ Assets/Scripts/Pigeon.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum LifeStage
+{
+    Egg,
+    Chick,
+    Young,
+    Adult
+
+}
+
+
+public class Pigeon 
+{
+    public string pigeonID;
+
+
+
+
+
+}
