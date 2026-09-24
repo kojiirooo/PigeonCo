a848ae1b561fce81baca6a9e707f9ee0c5e59f6a
johnrich9930 <johnrichadante45@gmail.com>
2026-09-24 19:08:53 +0800

    added constructors to pigeon.cs

--- Assets/Scripts/Pigeon.cs
+++ Assets/Scripts/Pigeon.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 
 public enum LifeStage
@@ -16,12 +17,26 @@ public class Pigeon
 {
     public string pigeonID;
     public string pigeonName;
-    public string color;
+    public string pigeonColor;
     public string breedType;
     public LifeStage stage;
     public float hunger;
     public float bond;
     public System.DateTime lastFedTime;
     public float pigeonRating; 
+
+
+    public Pigeon (string id, string name, string color, string breed, LifeStage Stage, float Hunger, float Bond, System.DateTime lastfed, float rating )
+    {
+        pigeonID = id;
+        pigeonName = name;
+        pigeonColor = color;
+        breedType = breed;
+        stage = Stage;
+        hunger = Hunger;
+        bond = Bond;
+        lastFedTime = lastfed;
+        pigeonRating = rating;
+    }
 
 }
